0bfde9b824109accb14290f539ab80391c035826
Dhwaj Jain <86903982+Dije-7@users.noreply.github.com>
2023-04-05 22:38:00 +0530

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/Projects', title: 'Projects' },
   { href: '/about', title: 'About' },
-  { href: '/uses', title: 'Uses' },
+  { href: '/uses', title: 'Contact' },
 ]
 
 export default headerNavLinks
